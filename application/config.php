4007234340250899d6fbc04538749f7408d8e644
captain-redbeard <troy.ross@hosvir.com>
2017-03-19 14:44:42 +1000

    Updated config

--- application/config.php
+++ application/config.php
@@ -6,18 +6,24 @@
 
 return [
     'app' => [
-        'base_dir' =>           __DIR__,
-        'config_directory' =>   __DIR__ . '/Config/',
-        'timezone' =>           'UTC',
-        'user_session' =>       'redbeard_user',
-        'user_role' =>          10,
-        'password_cost' =>      12,
-        'max_login_attempts' => 5,
-        'secure_cookies' =>     false,
-        'token_expire_time' =>  900,
-        'system_path' =>        '\\Redbeard\\Crew\\',
-        'path' =>               '\\Demo\\',
-        'default_controller' => 'Home',
-        'default_method' =>     'index'
+        'base_dir' =>               __DIR__,
+        'config_directory' =>       __DIR__ . '/Config/',
+        'models_directory' =>       __DIR__ . '/Models/',
+        'views_directory' =>        __DIR__ . '/Views/',
+        'controllers_directory' =>  __DIR__ . '/Controllers/',
+        'models_path' =>            'Models\\',
+        'views_path' =>             'Views\\',
+        'controllers_path' =>       'Controllers\\',
+        'system_path' =>            '\\Redbeard\\Crew\\',
+        'path' =>                   '\\Demo\\',
+        'default_controller' =>     'Home',
+        'default_method' =>         'index'
+        'timezone' =>               'UTC',
+        'user_session' =>           'redbeard_user',
+        'user_role' =>              10,
+        'password_cost' =>          12,
+        'max_login_attempts' =>     5,
+        'secure_cookies' =>         false,
+        'token_expire_time' =>      900
     ]
 ];
